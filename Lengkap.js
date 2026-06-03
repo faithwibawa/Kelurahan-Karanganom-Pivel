4698e11a7a81cb904f2199c402de311ce8136447
faithwibawa <faithwibawa@gmail.com>
2026-06-03 10:22:51 +0700

    Add files via upload

---
+++ Lengkap.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+    setTimeout(() => {
+        document.getElementById("bab1").classList.add("muncul");
+        document.getElementById("bab2").classList.add("muncul2");
+    }, 500);
+};
